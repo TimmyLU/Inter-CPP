0ec5141b43a1e61908ebf7607f09ca3af6945556
TimmyLU <51018765+TimmyLU@users.noreply.github.com>
2024-06-28 14:25:59 +0800

    Add files via upload

---
+++ i++&++i.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	
+	int a[] = {1, 2, 3, 4, 5, 6};
+    int *p = a;
+    *(p++) += 100;
+    *(++p) += 100;
+    for(int i=0; i<6; i++){
+        printf("%d ", a[i]); // Ans : 101 2 103 4 5 6
+    }
+    return 0;
+}
