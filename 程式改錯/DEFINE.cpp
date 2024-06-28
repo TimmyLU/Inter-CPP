eedf1ae166333ccaa9cc4a1d3370b6f19809f87c
TimmyLU <51018765+TimmyLU@users.noreply.github.com>
2024-06-28 15:01:54 +0800

    Update DEFINE.cpp

--- 程式改錯/DEFINE.cpp
+++ 程式改錯/DEFINE.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
 	cout << a*a << endl; //預期結果為25 but ANS = 11
-						 //原因 2+3*2+3 會先處理乘法運算
-						 //#define改成 (2+3) 即可
+			     //原因 2+3*2+3 會先處理乘法運算
+			     //#define改成 (2+3) 即可
 	return 0;
 }
