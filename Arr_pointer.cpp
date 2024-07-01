7d94d4544963a913189180d207e8a22e4496eda7
TimmyLU <51018765+TimmyLU@users.noreply.github.com>
2024-07-01 20:26:50 +0800

    Add files via upload

---
+++ Arr_pointer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	int a[] = {6, 7, 8, 9, 10};
+	int leng = sizeof(a)/sizeof(int);
+	int *p = a;
+	*(p++) += 123;
+	*(++p) += 123;
+	for(int j = 0;j<leng;j++){
+		cout << a[j] << " ";
+	}
+}
