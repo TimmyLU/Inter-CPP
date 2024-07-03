b5aec4a096c4e2118ff56b0581fea52685646166
TimmyLU <51018765+TimmyLU@users.noreply.github.com>
2024-07-03 18:02:15 +0800

    Update Bitwise of C.cpp

--- Bitwise of C.cpp
+++ Bitwise of C.cpp
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> //問 output?
 int main(){
-	unsigned int x = 0xf;
-	x <<= 4;
-	x |= 0x03;
+	unsigned int x = 0xf; //0x0000000f
+	x <<= 4; // 0x0000000f ---> 0x000000f0
+	x |= 0x03; // x = x | 0x03 ----> 0x000000f3
 	printf("0x%x", x); // output = 0xf3
 }
