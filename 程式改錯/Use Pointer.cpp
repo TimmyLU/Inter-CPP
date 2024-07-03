82cab8a664638dcb1d2896988b75369f3c1d8f92
TimmyLU <51018765+TimmyLU@users.noreply.github.com>
2024-07-03 18:55:41 +0800

    Update Use Pointer.cpp

--- 程式改錯/Use Pointer.cpp
+++ 程式改錯/Use Pointer.cpp
@@ -6,12 +6,12 @@ void swap(int a, int b){
     int temp = a;
     a = b;
     b = temp;
-    //cout << a << " " << b << endl;
+    //cout << a << " " << b << endl; // a = 10, b = 5;
 }
 
 int main(){
     int a = 5;
     int b = 10;
     swap(a, b);
-    cout << a << " " << b << endl;
+    cout << a << " " << b << endl; // // a = 5, b = 10;
 }
