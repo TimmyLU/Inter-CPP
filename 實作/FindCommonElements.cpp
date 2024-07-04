9a321e5a6d897d3898c862c5808b361e07c5d948
TimmyLU <51018765+TimmyLU@users.noreply.github.com>
2024-07-04 23:01:38 +0800

    Update FindCommonElements.cpp

--- 實作/FindCommonElements.cpp
+++ 實作/FindCommonElements.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 void findCommonElements(vector<int> &arr1, vector<int> &arr2){
-    sort(arr1.begin(), arr1.end());
-    sort(arr2.begin(), arr2.end());
+    //sort(arr1.begin(), arr1.end());
+    //sort(arr2.begin(), arr2.end());
     
     for(int i = 0; i < arr1.size(); i++){
         for(int j = 0; j < arr2.size(); j++){
