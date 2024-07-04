59602c98e8dabb45ee3734f8347d99cdf47e4ffb
TimmyLU <51018765+TimmyLU@users.noreply.github.com>
2024-07-04 15:14:10 +0800

    Add files via upload

---
+++ 實作/Convert.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+char convert(char c) {
+    if (c >= 'A' && c <= 'Z')
+        return c + 32;
+    else if (c >= 'a' && c <= 'z')
+        return c - 32;
+    else
+        return c;
+}
+
+int main(){
+    char c;
+    cin >> c;
+    cout << convert(c);
+}
