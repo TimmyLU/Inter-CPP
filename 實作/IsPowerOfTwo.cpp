833b31b2816bad2ca62b886b24b56b694ed5d5d5
TimmyLU <51018765+TimmyLU@users.noreply.github.com>
2024-07-12 18:23:34 +0800

    Update IsPowerOfTwo.cpp

--- 實作/IsPowerOfTwo.cpp
+++ 實作/IsPowerOfTwo.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 bool isPowerOfTwo(int num){
-    return (num > 0 && (num & (num - 1)) == 0) ? "true" : "false";
+    return num > 0 && (num & (num - 1)) == 0;
 }
 int main(){
 	int num;
 	while(cin >> num){
-		if(isPowerOfTwo){
+		if(isPowerOfTwo(num)){
 		    cout << "yes";
 		}else{
 		    cout << "no";
